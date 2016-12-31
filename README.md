d22b1df46a16255e27b0efdd35045804f5fcd9dd
Dibyendu <dibyendu.coder@gmail.com>
2016-12-31 12:03:47 -0800

    Day 1: Datatypes

--- README.md
+++ README.md
@@ -3,3 +3,4 @@
 ## Begining my daily routine of coding 1 problem per day
 
 ### Day 0: Hello World
+### Day 1: Datatypes
---
+++ day1_datatypes.py
@@ -0,0 +1,16 @@
+
+# Declare second integer, double, and String variables.
+j = input()
+e = input()
+t = raw_input()
+# Read and save an integer, double, and String to your variables.
+
+# Print the sum of both integer variables on a new line.
+su = j+i
+# Print the sum of the double variables on a new line.
+print su
+# Concatenate and print the String variables on a new line
+ku = d+e
+# The 's' variable above should be printed first.
+print ku
+print s+t
